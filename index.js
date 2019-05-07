d13265c9f2dd26f77273881d93e0e95d207ff6ba
Marco Frisina <marco@frisina.xyz>
2019-05-07 09:59:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,11 +11,30 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var itemObject = new Object();
+
+ itemObject.itemName = item;
+ itemObject.itemPrice = Math.floor(Math.random() * 101);
+
+ cart.push(itemObject);
+ return `${itemObject.itemName} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  var text = "In your cart, you have ";
+  if (cart.length === 0) {
+    var text = "Your shopping cart is empty.";
+    return text;
+  } else
+    { for(var i = 0; i < cart.length - 1; i++) {
+    text += itemObject.itemName[0] + " at " + itemObject.itemPrice + ",";
+    }
+    text += cart.length + " + " + itemObject.itemPrice + ".";
+    return text;
+    }
 }
+
+
 
 function total() {
   // write your code here
