100cebaa51ffd212ea398663fcb3816cdd651aab
Marco Frisina <marco@frisina.xyz>
2019-05-07 10:57:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function viewCart() {
   if (cart.length === 0) {
     return "Your shopping cart is empty.";
   } else if (cart.length === 1) {
-    text += text + ` ${cart[0].itemName} at $${cart[0].itemPrice}.`
+    text = text + ` ${cart[0].itemName} at $${cart[0].itemPrice}.`
     //var oneItem = `In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])}`;
     return text;
   }
